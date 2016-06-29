896bfa0d9fa4714a7043f6b05a83fe09aaed5935
chenjian <jesse@kibey.com>
2016-06-29 11:14:41 +0800

    Add Option

--- src/main/scala/chapter4/Option.scala
+++ src/main/scala/chapter4/Option.scala
@@ -26,10 +26,18 @@ sealed trait Option[+A] {
 
   def orElse[B >: A](ob: => Option[B]): Option[B] = {
     this match {
-
+      case None => ob
+      case _ => this
     }
   }
-  def filter(f: A => Boolean): Option[A]
+
+  def filter(f: A => Boolean): Option[A] = {
+    this match {
+      case Some(a) if f(a) => this
+      case _ => None
+    }
+  }
+
 }
 
 case class Some[+A](get:A) extends Option[A]
