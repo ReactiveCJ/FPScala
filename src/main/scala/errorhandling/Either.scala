9a678661a0fab7019862092f121f14d52842c21c
chenjian <jesse@kibey.com>
2016-09-12 16:23:48 +0800

    Add Option

--- src/main/scala/errorhandling/Either.scala
+++ src/main/scala/errorhandling/Either.scala
@@ -97,21 +97,6 @@ object test extends App{
 
   import Either._
 
-  case class Person(name: Name, age: Age)
-  sealed case class Name( value: String)
-  sealed case class Age( value: Int)
-
-  def mkName(name: String): Either[String, Name] =
-    if (name == "" || name == null) Left("Name is empty.")
-    else Right(new Name(name))
-
-  def mkAge(age: Int): Either[String, Age] =
-    if (age < 0) Left("Age is out of range.")
-    else Right(new Age(age))
-
-  def mkPerson(name: String, age: Int): Either[String, Person] =
-    mkName(name).map2(mkAge(age))(Person)
-
   def insure(age:String,speed:String):Either[Exception,Double] = {
     Try(age.toDouble).map2(Try(speed.toDouble))(_*_)
   }
